a180d8e52abf72616fa7e89f2557d51c515c50b6
jbj950606 <jbj950606@gmail.com>
2019-05-16 09:37:41 +0900

    2019-05-16 0937 최종 ver

---
+++ C_Study_basic/1_CLI/3_function/2_precaution/3_t_function_proto_type2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 함수 원형(Function Prototype)
+void my_add(void);
+
+void main(void){ 
+	my_add();
+}
+
+void my_add(void){ 
+	int number1;
+	int number2;
+	int result;
+
+	printf("두 수를 입력하세요 : ");
+	scanf("%d %d", &number1, &number2);
+
+	printf("\n- 간단한 덧셈 연산 ver1 \n");
+	printf("첫 번째 입력 : %d \n", number1);
+	printf("두 번째 입력 : %d \n", number2);
+	
+	result = number1 + number2;
+	printf("%d + %d = %d \n", number1, number2, result);
+}
+
